8d0150a709c28b9d887f98465e614ccf20932947
Jaycee Mariano <jayceedaily@gmail.com>
2021-03-18 22:06:15 +0800

    add created_by for form

--- database/factories/FormFactory.php
+++ database/factories/FormFactory.php
@@ -3,6 +3,7 @@
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
 use App\Models\Form;
+use App\Models\User;
 use Faker\Generator as Faker;
 
 $factory->define(Form::class, function (Faker $faker) {
@@ -12,5 +13,6 @@ $factory->define(Form::class, function (Faker $faker) {
         'has_random_questions' => $faker->boolean(),
         'accept_answers' => $faker->boolean(),
         'created_at' => $faker->dateTime(),
+        'created_by' => User::inRandomOrder()->first()
     ];
 });
