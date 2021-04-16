2767f321d4286814ca2c6ea4cae5d3b101118bb4
Jaycee Mariano <alohajaycee@gmail.com>
2021-04-16 21:21:18 +0800

    implement searchbales

--- app/Models/Form.php
+++ app/Models/Form.php
@@ -2,8 +2,17 @@
 
 namespace App\Models;
 
+use App\Models\Traits\HasSearchable;
+
 class Form extends Model
 {
+    use HasSearchable;
+
+    protected $searchable = [
+        'questions.content',
+        'author.name',
+        'name'];
+
     protected $fillable = ['name', 'description', 'has_random_questions'];
 
     public function questions()
@@ -14,7 +23,6 @@ class Form extends Model
     public function randomized_questions()
     {
         return $this->hasMany('App\Models\Question')->inRandomOrder();
-
     }
 
     public function sheets()
