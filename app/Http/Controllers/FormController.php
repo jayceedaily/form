6f2e7a685b1090f4189ff0fc0f87f21506e2bc95
Jaycee Mariano <jayceedaily@gmail.com>
2021-04-17 23:04:59 +0800

    fix filter typo

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -29,7 +29,7 @@ class FormController extends Controller
         }
 
         if( $request->has('filter') &&
-            is_array($request->filters)) {
+            is_array($request->filter)) {
 
             $query->filter($request->filter);
         }
