26f00bda45f6029c42ebf1a2c9baaa3120aa3bae
Jaycee Mariano <jayceedaily@gmail.com>
2021-04-17 23:06:39 +0800

    add search field

--- resources/js/view/form/Index.vue
+++ resources/js/view/form/Index.vue
@@ -2,10 +2,16 @@
 <div class="container">
     <div class="columns">
         <div class="column">
+            <form action="" @submit.prevent="handleSearch">
+                <input type="text" v-model="searchQuery" name="" id="" class="input">
+            </form>
+        </div>
+    </div>
+    <div class="columns">
+        <div class="column">
             <!-- <button class="button is-primary">Create form</button> -->
             <router-link href="#" class="button is-primary" to='form/create'>Create form</router-link>
             <button @click="showFilterModal=true" class="button is-pulled-right"><i class="material-icons">filter_list</i></button>
-
         </div>
     </div>
     <div class="columns">
@@ -22,7 +28,7 @@
 <script>
 import TheTable from './TheTable';
 import FilterModal from './FilterModal';
-import { mapActions } from 'vuex';
+import { mapActions, mapGetters } from 'vuex';
 export default {
     name: 'Form',
     components:{
@@ -31,7 +37,8 @@ export default {
     },
     data: function() {
         return {
-            showFilterModal: false
+            showFilterModal: false,
+            searchQuery: "",
         }
     },
     watch: {
@@ -48,6 +55,11 @@ export default {
             immediate: true
         }
     },
+    computed: {
+        ...mapGetters({
+            formFilters: 'form/filters',
+        })
+    },
 
     created: function() {
 
@@ -59,6 +71,13 @@ export default {
             loadForms:'form/load',
             formSetFilter: 'form/setFilter'
         }),
+
+        handleSearch() {
+            let _filters = {search: this.searchQuery};
+
+
+            this.formSetFilter(_filters)
+        }
     }
 }
 </script>
